860edc3924705e35069990a40fe9fe3b24737711
parenz22 <pierce.arenz@mail.wlc.edu>
2026-04-24 09:06:07 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+
+import streamlit as st
+
+
+st.title('My First Streamlit App')
+
+st.write('Hello! This app is live on Streamlit Community Cloud.')
+
+
+name = st.text_input('Enter your name:')
+
+if name:
+
+st.write(f'Welcome, {name}!')
